a11c9e8938b356b6dab5f4ef8bd58008900c92f5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-05-22 16:13:52 +0200

    Deploy to firebase hosting

---
+++ scripts/deploy-app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Go to the project root directory
+cd $(dirname ${0})/..
+
+# Build the help in AOT and production mode.
+$(npm bin)/ng build --aot -prod
+
+# Upload app to firebase
+$(npm bin)/firebase deploy
